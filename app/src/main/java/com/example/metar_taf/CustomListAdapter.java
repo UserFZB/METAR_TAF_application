b57765f8833f4527402703eb393195bae5b00958
Bouharba <bouharba.fatimazahra@gmail.com>
2021-11-07 16:12:49 +0100

    Activity search layout, List Adapter

---
+++ app/src/main/java/com/example/metar_taf/CustomListAdapter.java
@@ -0,0 +1,53 @@
+package com.example.metar_taf;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomListAdapter extends BaseAdapter implements ListAdapter {
+
+    private ArrayList<String> list = new ArrayList<String>();
+    private Context context;
+
+    public CustomListAdapter(ArrayList<String> list, Context context){
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if(view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_items, null);
+        }
+
+        TextView code = (TextView)view.findViewById(R.id.airport_code);
+        code.setText(list.get(position));
+
+
+        return view;
+    }
+}
